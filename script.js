ef625be52dfa5cfd8167e235e798702a81e6b4ff
acharyabidhan <acharyaumesh2005@gmail.com>
2023-07-30 11:13:02 +0545

    some improvements

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
 "use strict";
 
+function get_zindex(element, zindex = 0) {
+    const parent_elem = element.parentElement;
+    if (parent_elem === null) return zindex;
+    zindex += 1
+    return get_zindex(parent_elem, zindex);
+}
+
+function countPreviousSiblings(element) {
+    let count = 0;
+    let prevSibling = element.previousElementSibling;
+    while (prevSibling) {
+        count++;
+        prevSibling = prevSibling.previousElementSibling;
+    }
+    return count;
+}
+
+
 class CustomSelectOption {
-    constructor(parent, options, callback, _sort = false) {
+    constructor(parent, options, callback, _sort = false, max_width = "300px", max_height = "300px") {
         this.wrapper_container = document.querySelector(parent);
         this.all_options_list = _sort ? options.sort() : options;
         this.on_value_change = callback;
         this.selected_value = null;
+        this.max_width = max_width;
+        this.max_height = max_height;
+        this.wrapper_container_zindex = get_zindex(this.wrapper_container);
+        this.no_of_prev_elements = countPreviousSiblings(this.wrapper_container);
         this.create_new_select_option();
     }
-    get_element_by_id(id) { return document.getElementById(id); }
-    get_selected_value() { return this.selected_value; }
+    get_element_by_id(id) {
+        return document.getElementById(id);
+    }
+    get_selected_value() {
+        return this.selected_value;
+    }
     create_new_select_option() {
         const main_option_container = document.createElement("div");
-        main_option_container.style.width = "300px";
+        main_option_container.style.maxWidth = this.max_width;
         main_option_container.style.position = "relative";
+        main_option_container.style.zIndex = this.wrapper_container_zindex - this.no_of_prev_elements;
         const input_field_wrapper = document.createElement("div");
-        input_field_wrapper.id = "main-options-container"
+        input_field_wrapper.className = "main-options-container";
         const option_input = document.createElement("input");
         option_input.style.cursor = "pointer";
         option_input.style.width = "100%";
         option_input.placeholder = "Choose option";
-        option_input.id = "option-input-field"
+        option_input.className = "option-input-field";
         input_field_wrapper.appendChild(option_input);
         const options_container = document.createElement("div");
         options_container.style.position = "absolute";
-        options_container.style.maxHeight = "300px";
+        options_container.style.maxHeight = this.max_height;
         options_container.style.width = "100%";
         options_container.style.overflow = "auto";
         options_container.style.display = "none";
-        options_container.id = "options-list-container";
+        options_container.className = "options-list-container";
         main_option_container.append(input_field_wrapper, options_container);
         this.wrapper_container.appendChild(main_option_container);
         let opt_div;
@@ -37,7 +64,7 @@ class CustomSelectOption {
             opt_div.innerText = value;
             opt_div.style.borderBottom = "1px solid gray";
             opt_div.style.cursor = "pointer";
-            opt_div.id = "option-list";
+            opt_div.className = "option-list";
             options_container.appendChild(opt_div);
             opt_div.onclick = () => {
                 this.on_value_change(value);
@@ -47,23 +74,45 @@ class CustomSelectOption {
             };
         });
         const all_options_elem = Array.from(options_container.children);
-        function show_options() { options_container.style.display = "block"; }
-        function hide_options() { options_container.style.display = "none"; option_input.blur(); }
+        function show_options() {
+            options_container.style.display = "block";
+        }
+        function hide_options() {
+            options_container.style.display = "none";
+            option_input.blur();
+        }
         option_input.onfocus = show_options;
-        function display_options() { all_options_elem.forEach((option) => { option.style.display = "block"; }); }
+        function display_options() {
+            all_options_elem.forEach((option) => {
+                option.style.display = "block";
+            });
+        }
         option_input.onclick = display_options;
         option_input.oninput = () => {
             const input_value = option_input.value.toLowerCase();
             if (input_value != "") {
                 all_options_elem.forEach((option) => {
                     const option_value = option.textContent.toLowerCase();
-                    if (option_value.includes(input_value)) { option.style.display = "block"; }
-                    else { option.style.display = "none"; }
+                    if (option_value.includes(input_value)) {
+                        option.style.display = "block";
+                    } else {
+                        option.style.display = "none";
+                    }
                 });
-            } else { display_options(); }
-        }
-        document.addEventListener("keydown", (ev) => { if (ev.key == "Escape") { hide_options() } })
-        document.addEventListener("click", (e) => { if (e.target != opt_div && e.target != option_input) { hide_options(); } })
+            } else {
+                display_options();
+            }
+        };
+        document.addEventListener("keydown", (ev) => {
+            if (ev.key == "Escape") {
+                hide_options();
+            }
+        });
+        document.addEventListener("click", (e) => {
+            if (e.target != opt_div && e.target != option_input) {
+                hide_options();
+            }
+        });
     }
 }
 
@@ -84,5 +133,5 @@ function callback_f2(value) {
     console.log(value);
 }
 
-const select_option1 = new CustomSelectOption(".container", brand_list, callback_f1, true);
-const select_option2 = new CustomSelectOption(".container", country_list, callback_f2, false);
+const select_option1 = new CustomSelectOption(".option1", brand_list, callback_f1, true);
+const select_option2 = new CustomSelectOption(".option2", country_list, callback_f2, false);
